87a498ff2f37edeff60f738b4f08f1b71fba58e7
Jarom Bridges <jarombridges@hotmail.com>
2019-12-02 12:56:19 -0700

    rolled back to initial login/logout function

--- src/App.js
+++ src/App.js
@@ -10,30 +10,10 @@ import {
 import {clearCookies} from './Components/Logout'
 
 class App extends Component {
-  state = {
-    user: 'guest'
-  }
-  async componentDidMount() {
-    
-    const user = await sessionStorage.getItem('user')
-
-    const checkUser = () => {
-      if (user) {
-      this.setState({
-        user: user
-      })    
-  }
-    checkUser()
-  }
-    
- }
 
   logout = () => {
     sessionStorage.clear();
     clearCookies();
-    this.setState({
-      user: 'guest'
-    })
   }
 
   render() {
@@ -46,11 +26,8 @@ class App extends Component {
             <h1 style = {{fontSize: 50, fontWeight: 'bold'}}><Link style ={{textDecoration: 'none'}} to="/home">Stuff to Eat</Link></h1>
               <ul style={{listStyle: 'none', padding: 0, display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'center', color: '#07AAFF', fontSize: 18}}>
                 <li><Link style ={{textDecoration: 'none'}} to="/">Home</Link></li>
-                {this.state.user === "guest" ?
                 <li><Link style ={{textDecoration: 'none'}} to="/user/login">Login</Link></li>
-                :
                 <li><Link onClick={this.logout}  style ={{textDecoration: 'none'}} to="/">Logout</Link></li>
-                }
               </ul>
             </nav>   
             <Switch>
